5bf205a9f8a13ce0f90fd649c37353a90d813b81
Anuroshan B <anuroshan886@gmail.com>
2023-09-17 13:39:06 +0530

    new modification

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let outputScreen=document.getElementById("output-screen");
         function display(num){
             outputScreen.value += num;
         }
-
+    //calculating user defined function for equal button
         function Calculate(){
             try{
                 outputScreen.value = eval(outputScreen.value);
@@ -12,11 +12,11 @@ let outputScreen=document.getElementById("output-screen");
                 alert("Invalid")
             }
         }
-
+    //clear user defined function for cl button.this will use to clear the whole screen text on input box
         function Clear(){
             outputScreen.value="";
         }
-
+    //del user defined function for button.this funcion is used to delet the text one by one 
         function del(){
             outputScreen.value=outputScreen.value.slice(0,-1);
         }
